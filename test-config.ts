2d73f75f81b53963f1085cce782968f79b9c7f27
tungzuizer <tunghb2007@gmail.com>
2026-09-24 11:49:16 +0700

    chore: clean repository and ignore non-operational files

--- test-config.ts
+++
@@ -1,3 +0,0 @@
-import config from "./prisma/prisma.config";
-console.log("Config loaded successfully:");
-console.log(JSON.stringify(config, null, 2));
--- writer.js
+++
@@ -1,1 +0,0 @@
-const fs = require('fs'); const content = fs.readFileSync(0, 'utf-8'); fs.writeFileSync(process.argv[2], content, 'utf-8');
